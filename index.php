b3018209ebd6e3477631d724b9d3fcf709834a7f
Christopher Collopy <chelco91@gmail.com>
2015-09-21 20:00:40 -0600

    created templated index file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php $PAGE_TITLE = "Responsive Mobile First Design";
+		require_once("lib/templates/head-utils.php"); ?>
+	</head>
+
+	<body>
+		<header>
+			<?php require_once("lib/templates/header.php"); ?>
+		</header>
+
+		<main>
+
+		</main>
+
+		<footer>
+			<?php require_once("lib/templates/footer.php"); ?>
+		</footer>
+	</body>
+</html>
